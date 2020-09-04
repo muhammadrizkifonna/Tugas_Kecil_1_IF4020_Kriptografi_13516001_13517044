5b90bd86af41702f9add2111c0d2afff75bc0795
Muhammad Rizki Fonna <13516001@std.stei.itb.ac.id>
2020-09-05 01:27:54 +0700

    Make vigenere cipher encryption (almost finished)

--- vigenere.py
+++ vigenere.py
@@ -9,7 +9,7 @@ key = input("Masukkan key:")
 
 #print(key)
 
-#Compare plaintext and key's length
+#Compare plaintext and key's length#
 if (key<plaintext):
     real_key=key
     times = len(plaintext)//len(key)
@@ -22,9 +22,29 @@ if (key<plaintext):
     for i in range(sisa):
         key+=real_key[i]
 
+#If key>plaintext (has not finished)#
+
+#Alphabet list for conversion from number to character#
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',\
+             'w', 'x', 'y', 'z']
+
+#Conversion from plaintext to ciphertext#
+
+def plain_to_cipher(plaintext, key):
+    #Alphabet list for conversion from number to character#
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',\
+             'w', 'x', 'y', 'z']
+    ciphertext=''
+    for i in range(len(plaintext)):
+        ciphernumber=(alphabet.index(plaintext[i])+alphabet.index(key[i]))%26
+        #print(ciphernumber, end=', ')
+        ciphertext+=alphabet[ciphernumber]
+        #print(ciphertext)
+    return ciphertext
+
+#Outputs the ciphertext#
+print("Ciphertext:"+plain_to_cipher(plaintext, key))
 
 
 
 
-
-print(key)
